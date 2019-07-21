685e2fcbdd5af0c8ca4d536a9adfd108b5499913
Andrew Kim <akinwahn@gmail.com>
2019-07-21 01:04:44 -0400

    dist static build

---
+++ dist/precache-manifest.498154e39a25cb1038c1604424b0b5b6.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "c14b97f2006b4db8e34b",
+    "url": "/css/app.0a424798.css"
+  },
+  {
+    "revision": "c14b97f2006b4db8e34b",
+    "url": "/js/app.4f96bdbb.js"
+  },
+  {
+    "revision": "25e2de899d00e985285c",
+    "url": "/js/chunk-vendors.b2285bd2.js"
+  },
+  {
+    "revision": "640fde7c7a5915462497",
+    "url": "/js/common.ed39a86f.js"
+  },
+  {
+    "revision": "86ceca4a4a3248e8da58",
+    "url": "/js/dashboard.a90b44d2.js"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/fonts/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/fonts/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/fonts/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/fonts/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "04e8e2548e9272e99ca1e0bdf44049a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82afbfaa2a64cebfd0077f73abe15659",
+    "url": "/apple-icon.png"
+  },
+  {
+    "revision": "d2caaad36a395104bd99f30487798d79",
+    "url": "/img/apple-icon.png"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "3b91359dcf1cf96c8440a7532ac61dda",
+    "url": "/img/anime3.png"
+  },
+  {
+    "revision": "d25412bcd9af1fb98017c8144a4e40d1",
+    "url": "/img/default-avatar.png"
+  },
+  {
+    "revision": "4422794dfa779751326660ae681b082e",
+    "url": "/img/favicon.png"
+  },
+  {
+    "revision": "2d1ae900a567114628111e96e4d4ccbd",
+    "url": "/img/francis.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2a5a031c63d290c6cd3803e21d8c3b5d",
+    "url": "/img/anime6.png"
+  },
+  {
+    "revision": "c2cc1076999142be6b82337f62709fb3",
+    "url": "/img/pulse-logo.png"
+  },
+  {
+    "revision": "7ac95108c92e367c69b5d2f95528610c",
+    "url": "/img/blueprint.png"
+  },
+  {
+    "revision": "8bf66721349605aaaa5acb096113e29f",
+    "url": "/img/dpts.png"
+  },
+  {
+    "revision": "fadc3f3a4c94b151d8b8ed33456c2344",
+    "url": "/img/emilyz.jpg"
+  },
+  {
+    "revision": "f220d1815d421bfa192ee352780ba194",
+    "url": "/img/img_3115.jpg"
+  },
+  {
+    "revision": "93f1328bd264f02aacef8b0c6b498445",
+    "url": "/img/mike.jpg"
+  },
+  {
+    "revision": "40fdb6e1e72e21ff904da682c46d0116",
+    "url": "/img/james.jpg"
+  },
+  {
+    "revision": "f6d6740ed5731a15ec040f2a0a6dcc93",
+    "url": "/img/maryann.png"
+  },
+  {
+    "revision": "9b0c49b657dd738a77a37e7dabcc0b5d",
+    "url": "/img/bg5.jpg"
+  }
+];
